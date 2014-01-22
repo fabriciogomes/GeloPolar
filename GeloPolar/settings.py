e83edf6705e1e1ba8e6c695887732110ccf88cb7
fabriciogomes <fabriciogomesfelix@gmail.com>
2014-01-22 18:24:56 -0200

    Update settings.py
    configurando banco

--- GeloPolar/settings.py
+++ GeloPolar/settings.py
@@ -12,12 +12,12 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/home/fabricio/workspace/GeloPolar/sqlite.db',                      # Or path to database file if using sqlite3.
+        'NAME': '/home/fabricio/workspace/GeloPolar/banco.db',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
+        'USER': 'root',
+        'PASSWORD': 'felix',
+        'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '3306',                      # Set to empty string for default.
     }
 }
 
